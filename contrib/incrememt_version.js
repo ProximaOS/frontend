4da18e86c6ae35d5c3a9fa216a0dab09e12d6e01
mortada-killer0 <mcsplayer914@gmail.com>
2023-08-21 14:37:22 +0300

    Build release changes

---
+++ contrib/incrememt_version.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+const packageJsonPath = 'package.json';
+const packageJson = require(packageJsonPath);
+
+const [major, minor, patch, build] = packageJson.version.split('.');
+const newBuild = parseInt(build) + 1;
+
+packageJson.version = `${major}.${minor}.${patch}.${newBuild}`;
+
+fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
