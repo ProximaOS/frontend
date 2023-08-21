d39566d6097040f7c46ab017ee18741aa169c655
mortada-killer0 <mcsplayer914@gmail.com>
2023-08-21 21:30:16 +0300

    Version incrementer updated

--- contrib/increment_version.js
+++ contrib/increment_version.js
@@ -1,11 +1,17 @@
 const fs = require('fs');
 
-const packageJsonPath = '../package.json';
-const packageJson = require(packageJsonPath);
+const packageJsonPath = 'path/to/your/package.json';
+const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
 
-const [major, minor, patch, build] = packageJson.version.split('.');
-const newBuild = parseInt(build) + 1;
+const currentVersion = packageJson.version;
+const preReleaseTag = currentVersion.split('-')[1]; // Extract pre-release tag
 
-packageJson.version = `${major}.${minor}.${patch}.${newBuild}`;
+// Increment the pre-release tag number
+const tagNumber = parseInt(preReleaseTag.substring(1)); // Remove the 'b' prefix and convert to number
+const newTagNumber = tagNumber + 1;
+
+const newVersion = currentVersion.replace(preReleaseTag, `b${newTagNumber}`);
+packageJson.version = newVersion;
 
 fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+console.log(`Updated version to: ${newVersion}`);
