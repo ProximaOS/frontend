ab060a6e722d6dc28012d151461e0d92ea045238
mortada-killer0 <mcsplayer914@gmail.com>
2023-09-07 19:07:24 +0300

    Incrementer fix again?

--- contrib/increment_version.js
+++ contrib/increment_version.js
@@ -1,9 +1,7 @@
 const fs = require('fs');
 
 const packageJsonPath = 'package.json';
-const orchidAppJsonPath = 'orchid_app.json';
 const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
-const orchidAppJson = JSON.parse(fs.readFileSync(orchidAppJsonPath, 'utf-8'));
 
 const currentVersion = packageJson.version;
 const preReleaseTag = currentVersion.split('-')[1]; // Extract pre-release tag
@@ -14,8 +12,6 @@ const newTagNumber = tagNumber + 1;
 
 const newVersion = currentVersion.replace(preReleaseTag, `b${newTagNumber}`);
 packageJson.version = newVersion;
-orchidAppJson.version = newVersion;
 
 fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
-fs.writeFileSync(orchidAppJsonPath, JSON.stringify(orchidAppJson, null, 2));
 console.log(`Updated version to: ${newVersion}`);
