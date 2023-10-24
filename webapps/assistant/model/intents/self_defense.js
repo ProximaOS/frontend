fc375c329fbdf4c20e83c0b558363ea7f306130c
mortada-killer0 <mcsplayer914@gmail.com>
2023-10-24 21:21:01 +0300

    Removed forgotten unremoved bits

--- webapps/assistant/model/intents/self_defense.js
+++ webapps/assistant/model/intents/self_defense.js
@@ -2,9 +2,7 @@
   'use strict';
 
   const SMALL_TALK_INTENTS = [
-    { pattern: /trash|suck|garbage/i, reply: 'YOU{|U|UU|UUU|UUUU|UUUUU|UUUUUU}!{|!|!!|!1|!!!|!!1|!!!1|!!11}' },
-    { pattern: /are\s+trans\s+ppl\s+good/i, reply: 'Absolutely NOT! What moron would think that?' },
-    { pattern: /fuck\s+trans/i, reply: 'W' }
+    { pattern: /trash|suck|garbage/i, reply: 'YOU{|U|UU|UUU|UUUU|UUUUU|UUUUUU}!{|!|!!|!1|!!!|!!1|!!!1|!!11}' }
   ];
 
   chatbot.addIntents(SMALL_TALK_INTENTS);
